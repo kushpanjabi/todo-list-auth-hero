c6bd9cb84c7ed1e9eb3f24e30f1604544b5b606f
Kush Panjabi <kushpanjabi@icloud.com>
2020-06-05 13:34:39 -0700

    comment on index

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // allows req.body
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === 'production') {
     //server static content
