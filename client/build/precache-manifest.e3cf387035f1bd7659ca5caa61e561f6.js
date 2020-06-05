8ea4e5a6966bd0606838f73e987125aad3cba992
Kush Panjabi <kushpanjabi@icloud.com>
2020-06-05 13:30:07 -0700

    updt ignore file

---
+++ client/build/precache-manifest.e3cf387035f1bd7659ca5caa61e561f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741f1c3e020932f61d77488fe40b2acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db20e07a451520adac7",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d2de472f0fa9319b4aba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0db20e07a451520adac7",
+    "url": "/static/js/2.5d5049ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5d5049ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2de472f0fa9319b4aba",
+    "url": "/static/js/main.afa47d13.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
